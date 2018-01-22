28eafe86d493467043f461a11b3bc4707a9d320c
nivi.mor <nivi.mor@experitest.com>
2018-01-22 14:11:42 +0200

    first commit for tests

---
+++ README.md
@@ -0,0 +1,12 @@
+# This repository contains Selenium tests written in Python.
+## There are three tests:
+1. Chrome
+2. IE
+3. Firefox
+
+Make sure to install python on your machine and to install Selenium bindings:
+```pip install selenium```
+
+### Running the tests
+Example
+```python -m unittest test_chrome```
---
+++ test_chrome.py
@@ -0,0 +1,42 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
+
+class TestFirefox(unittest.TestCase):
+    dc = {}
+    testName = 'Selenium Test on chrome'
+    driver = None
+
+    def setUp(self):
+        self.dc['testName'] = self.testName
+        self.dc['browserName'] = 'chrome'
+        # Specify the access key in order to run this test against the browser Grid
+        self.dc['accessKey'] = ''
+        self.driver = webdriver.Remote('http://mastercloud/wd/hub', self.dc)
+
+    def test_chrome(self):
+        self.driver.get("https://seetest.io")
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.XPATH, "//*[text()='Manual']")))
+
+        manual_nav_link = self.driver.find_element_by_xpath("//*[text()='Manual']")
+        manual_nav_link.click()
+
+        automation_nav_link = self.driver.find_element_by_xpath("//*[text()='Automation']")
+        automation_nav_link.click()
+
+        webinar_footer_link = self.driver.find_element_by_xpath("//*[text()='Webinars']")
+        webinar_footer_link.click()
+
+        webinars_h2_title_text = self.driver.find_element_by_xpath("//h2[1]").text
+
+        print("The title of the first h2 is: " + webinars_h2_title_text)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    if __name__ == '__main__':
+        unittest.main()
